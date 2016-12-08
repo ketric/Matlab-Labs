c6a4ac2966fb624cbf9d9522fa8a300a297df9ea
ketric <kennyle1996@hotmail.com>
2016-12-08 22:13:11 +0100

    Lab 3

--- Uppgift2.m
+++ Uppgift2.m
@@ -1,28 +1,5 @@
-%%
-%A)
-format long
-n = 0;
-tol = 1e-5;
-s = 0;
-p = pi/4;
-while true
-    s = s +((-1)^n/(2*n+1));
-    
-    if abs(s - p) < tol
-        break
-    end
-        n = n + 1;
-end
-s1 = (pi/4) - s;
-disp ('Sum is:')
-disp (s)
+B1 = [4;3;1]
+B2 = [5;2;1]
+B3 = [6;1;1]
 
-
-%%
-%B)
-s = 0;
-for n = 0:999
-    s = s + ((-1)^n/(2*n+1));
-end
-disp ('pi/4 in 1000 terms is:')
-disp (s)
+B = [B1 B2 B3]
---
+++ Uppgift7.m
@@ -0,0 +1,13 @@
+A = [1 0 0; 0 1 0;]
+B = [1 0 0; -2 1 0; 0 0 1;]
+C = [2 1 1; 4 1 0; -2 2 1;]
+
+A*(B*C) = (A*B)*C  'Associative'
+
+A*(B+C) = AB + AC 'Distributive'
+
+(B+C)*A = BA + CA 'Distributive'
+
+AB = BA 'Commutative, B is the invers of Matrix A'
+
+
