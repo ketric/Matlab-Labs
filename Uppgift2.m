a55932f276220a7f6c419f51513af8933a7c0748
ketric <kennyle1996@hotmail.com>
2016-12-08 22:15:25 +0100

    Lab 6

--- Uppgift2.m
+++ Uppgift2.m
@@ -1,26 +1,22 @@
-H=[0 0.3 0 0.3 0 0.3 0 0.3; 0 0 0.3 0.3 0 0 0.3 0.3; 0 0 0 0 0.3 0.3 0.3 0.3];
-S=[1 2 4 3; 1 2 6 5; 1 3 7 5; 3 4 8 7; 2 4 8 6; 5 6 8 7];
-figure(1)
+%% Task 2
+clear 
+clc
+
+td = [1 2 3 4 5 6 7 8 9 10 11 12]';
+yd = [-0.9 -0.9 2.0 6.0 11.6 15.5 16.6 16.2 12.8 9.1 4.4 1.0]';
+
+A = [ones(size(td)) sin(30 * td) cos(30 * td)];
+x = A\yd;
+a = x(1);
+b = x(2);
+n=length(td);
+e = norm(A*x-yd)/sqrt(n);
+
 hold on
-H1 = zeros(size(S,1),4) ;
-H2 = zeros(size(S,1),4) ;
-H3 = zeros(size(S,1),4) ;
-axis equal, axis on, view(20,10)
-% Rotation along x axes 
-th = linspace(0,2*pi) ;
-for i = 1:length(th)
-    Rx = [1 0 0 ; 0 cos(th(i)) -sin(th(i)); 0 sin(th(i)) cos(th(i))] ;
-    % Rotate the vertices 
-    H1 = zeros(size(H)) ;
-    for j = 1:size(H,2)
-        H1(:,j) = Rx*H(:,j) ;
-    end
-    %translation
-    for k=1:size(S,1)    
-        Si=S(k,:); 
-        fill3(H1(1,Si),H1(2,Si),H1(3,Si),'g','facealpha',0.6)
-        hold on
-    end
-    drawnow
-    hold on
+for i = 1:12
+    scatter(td(i), yd(i),'filled') 
 end
+
+[p1,S] = polyfit(td,yd,7);
+plot(td, polyval(p1,td),'r--')
+grid on,hold off
