dd4b05c456a28221ef31e2e9c8052c22390037a0
ketric <kennyle1996@hotmail.com>
2016-12-08 22:23:50 +0100

    Lab 2

--- Uppgift1.m
+++ Uppgift1.m
@@ -1,25 +1,5 @@
-X=[0.1 0.8 0.9 0.1];
-Y=[0.2 0.1 0.7 0.2];
-
-fill(X,Y,'g','edgecolor','k','linewidth',1), hold on
-axis equal, axis([-1.5 2 -0.1 2]), pause(1)
-v=pi/6; 
-A=[cos(v) -sin(v); sin(v) cos(v)];
-P=[X;Y];
-for i=1:3
-P=A*P; % Varje koordinatpar roteras med vinkeln pi/6
-fill(P(1,:),P(2,:),'g','edgecolor','r','linewidth',1), pause(1)
+if 5>4
+    c=4
+else
+    c=5
 end
-plot(0,0,'ko','linewidth',2,'markersize',2) % origo
-hold off
-
-
-fill(X,Y,'g','edgecolor','k','linewidth',1), hold on
-axis equal, axis([-1.5 2 -0.1 2]), pause(1)
-t=[-0.6;0.3];
-P=[X;Y];
-for i=1:3
-P=P+t*ones(size(X));
-fill(P(1,:),P(2,:),'g','edgecolor','r','linewidth',1), pause(1)
-end
-hold off
