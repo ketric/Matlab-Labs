312d5dfd5e340fdc5bbd5ba9e232e01f88bfa240
ketric <kennyle1996@hotmail.com>
2016-12-08 22:12:31 +0100

    Lab 1

---
+++ uppgift1.m
@@ -0,0 +1,4 @@
+r = 4
+A = pi*r^2
+
+%definierar r som 4 och räknar ut arean
---
+++ uppgift3.m
@@ -0,0 +1,7 @@
+s=0;
+for i=1:5
+    s=s+i^2;
+end
+disp('summan är')
+disp(s)
+%counts the sum of 1^2 to 5^2 and then adds displays a text of the result 
