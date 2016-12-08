2a090da3d4d361d754b96921b12a87d026c68447
ketric <kennyle1996@hotmail.com>
2016-12-08 22:31:21 +0100

    Rename Uppgift5.m to Lab1Uppgift5.m

--- Lab1Uppgift5.m
+++ Lab1Uppgift5.m
@@ -1,13 +1,13 @@
 % a) 
 %Hur anger man antal punkter man vill ha?
 %y = linspace(x1,x2,n) genererar n punkter
-%Hur många punkter får man som standard
-%om man inte anger något antal?
+%Hur mÃ¥nga punkter fÃ¥r man som standard
+%om man inte anger nÃ¥got antal?
 %100
 
 % b)
 %x = fzero(fun,x0) tries to find a point x where fun(x) = 0.
-%This solution is where fun(x) changes sign—fzero
+%This solution is where fun(x) changes signâ€”fzero
 %cannot find a root of a function such as x^2.
 
 %x = fzero(fun,x0,options) uses options to modify the solution process.
